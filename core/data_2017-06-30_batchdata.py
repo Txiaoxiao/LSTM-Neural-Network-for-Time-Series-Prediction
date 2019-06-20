9f13e8e5748f72f2ecb6f628d6db28090d171843
Tang <13438872578@163.com>
2019-06-20 20:29:57 +0800

    write 2017_06_30_batchdata_updated_struct_errorcorrect.mat to csv file.header = ['cell','cycle_life','charge_policy','cycle','IR','QC','QD','Tavg','Tmin','Tmax','chargetime']

--- core/data_2017-06-30_batchdata.py
+++ core/data_2017-06-30_batchdata.py
@@ -27,8 +27,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import csv
+import pandas as pd
 
-def test():
+def main_struct():
     matFilename = 'H:\\Download\\2017-06-30_batchdata_updated_struct_errorcorrect.mat'
     f = h5py.File(matFilename, 'r+')
     print(f.keys())
@@ -71,9 +72,10 @@ def main():
         summary = {'IR': summary_IR, 'QC': summary_QC, 'QD': summary_QD, 'Tavg':
             summary_TA, 'Tmin': summary_TM, 'Tmax': summary_TX, 'chargetime': summary_CT,
                    'cycle': summary_CY}
+        # print(summary.keys())
         cycles = f[batch['cycles'][i, 0]]
         # cycle_dict = {}
-        # for j in range(cycles['I'].shape[0]):
+        # for j in range(cycles['I'].shape[0]):##此处有问题
         #     I = np.hstack((f[cycles['I'][j, 0]].value))
         #     Qc = np.hstack((f[cycles['Qc'][j, 0]].value))
         #     Qd = np.hstack((f[cycles['Qd'][j, 0]].value))
@@ -90,24 +92,60 @@ def main():
         key = 'b2c' + str(i)
         bat_dict[key] = cell_dict
     print(bat_dict.keys())
-    plt.plot(bat_dict['b2c0']['summary']['cycle'], bat_dict['b2c0']['summary']['QD'])
-    plt.show()
+    # plt.plot(bat_dict['b2c0']['summary']['cycle'], bat_dict['b2c0']['summary']['QD'])
+    # plt.show()
 
-#TODO:
-# write to csv file.
-# header: 都用float表示？
-# cell,cycle_life,charge_policy,cycle,IR,QC,QD,Tavg,Tmin,Tmax,chargetime
+# DONE:
+#  write to csv file.
+#  header = ['cell','cycle_life','charge_policy','cycle','IR','QC','QD','Tavg','Tmin','Tmax','chargetime']
+#  cell,cycle_life,cycle,charge_policy,IR,QC,QD,Tavg,Tmin,Tmax,chargetime
+#     csv_file='../data/2017_06_30_batchdatainrow.csv'
+    # writeDicToCsvInRows(csv_file, ',', bat_dict['b2c0']['summary'])
+    # writeDicToCsvInRows(csv_file,',',bat_dict)
+    csv_file='../data/2017_06_30_cell0_data.csv'
+    for i in range(len(bat_dict)):
+        csv_file='../data/2017_06_30_cell{0}_data.csv'.format(i)
+        cell_key = 'b2c'+str(i)
+        writeToCSV(csv_file,bat_dict[cell_key],i)
+
+    writeToCSV(csv_file,bat_dict['b2c0'],0)
+
+def writeToCSV(csv_file,cell_data,cel_num):
+
+    cell = cel_num;
+    cycle_life =int(cell_data['cycle_life'])
+    charge_policy = str(cell_data['charge_policy'])
+    summary = cell_data['summary']
+    cycle = summary['cycle']
+    IR = summary['IR']
+    QC = summary['QC']
+    QD = summary['QD']
+    Tavg = summary['Tavg']
+    Tmin = summary['Tmin']
+    Tmax = summary['Tmax']
+    chargetime = summary['chargetime']
+
+    r_num = cycle.size#row number
+
+    cell_list = list()
+    for i in range(r_num):
+        cell_list.append([cell,cycle_life,charge_policy,cycle[i],IR[i],QC[i],QD[i],Tavg[i],Tmin[i],Tmax[i],chargetime[i]])
+    header = ['cell','cycle_life','charge_policy','cycle','IR','QC','QD','Tavg','Tmin','Tmax','chargetime']
+
+    data = pd.DataFrame(columns=header,data=cell_list)
+    data.to_csv(csv_file,index=False)
 
 
-def writDicToCsv(csv_file,csv_columns,dict_data):
-    try:
-        with open(csv_file,'w') as csvfile:
-            writer = csv.DictWriter(csvfile,fieldnames=csv_columns)
-            writer.writeheader()
-            for data in dict_data:
-                writer.writerow(data)
-    except IOError as (erron,strerror):
-        print("I/O error({0}): {1}".format(erron,strerror))
+
+def writeDicToCsvInRows(csv_file, csv_columns, bat_dict):
+    with open(csv_file,'w') as csvfile:
+        for cell,cell_dict in bat_dict.items():
+            csvfile.write('cell,{0}\n'.format(cell))
+            csvfile.write('cycle_life,{0}\n'.format(cell_dict['cycle_life']))
+            csvfile.write('charge_policy,{0}\n'.format(cell_dict['charge_policy']))
+            dict_data = cell_dict['summary']
+            for k, v in dict_data.items():
+                csvfile.write('{0},{1}\n'.format(k, v.tolist()))
     return
 
 if __name__ == '__main__':
--- mytest.py
+++ mytest.py
@@ -1,4 +1,12 @@
 from core. utils import Transform
 
-filepath = 'data/B0005.mat'
-data = Transform().mat2csv(filepath)
+# filepath = 'data/B0005.mat'
+# data = Transform().mat2csv(filepath)
+import numpy as np
+# A = np.array([[2]])
+# print(A)
+# cell = np.pad(A,((0,0),(3,0)),'constant',constant_values=(2,2))
+# print(cell)
+
+dict = {'a':1,'b':2}
+print(len(dict))
